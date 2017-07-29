3ba4b88a585b25397631a763461882a0b5b1ccf7
Max Brauer <max@transcode.de>
2017-07-29 13:05:24 +0200

    Started model training.

--- ariane_core/__init__.py
+++ ariane_core/__init__.py
@@ -1,7 +1,33 @@
 # -*- coding: utf-8 -*-
-
 """Top-level package for Ariane Core."""
-
 __author__ = """Max Brauer"""
 __email__ = 'max@max-brauer.de'
 __version__ = '0.1.0'
+
+SUPPORTED_LANGUAGES = ['en', 'de']
+
+import click
+
+
+@click.group()
+@click.version_option(__version__)
+def cli():
+    pass
+
+
+@click.command()
+@click.option('--languages', '-l', multiple=True, default=SUPPORTED_LANGUAGES,
+    help='Languages to train. Defaults to {langs}'.format(
+        langs=SUPPORTED_LANGUAGES
+    )
+)
+def train(languages):
+    """Generate your trained model."""
+    for lang in languages:
+        if lang in SUPPORTED_LANGUAGES:
+            click.echo("Processing {lang}...".format(lang=lang))
+        else:
+            click.echo("{lang} not supported. Skipping...".format(lang=lang))
+
+
+cli.add_command(train)

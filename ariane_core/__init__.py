d9f0d612236cc52b05ffb65f81a7b7f54c02a75d
Max Brauer <max@transcode.de>
2017-07-30 20:30:09 +0200

    Started refactoring of training data.

--- ariane_core/__init__.py
+++ ariane_core/__init__.py
@@ -6,9 +6,11 @@ __version__ = "0.1.0"
 
 import datetime
 import os
+import importlib
 import json
 from pprint import pprint
 import shutil
+from tempfile import NamedTemporaryFile
 
 import click
 from langdetect import detect
@@ -38,8 +40,22 @@ def _get_config_path(lang):
     return os.path.join(_get_base_path(), "config/{lang}/config.json".format(lang=lang))
 
 
-def _get_training_data_path(lang):
-    return os.path.join(_get_base_path(), "config/{lang}/training_data.json".format(lang=lang))
+def _get_training_data_path(lang, config):
+    training_data = []
+    for app_name in config['active_apps']:
+        click.echo("Getting training data for {app}".format(app=app_name))
+        data_path = os.path.join(
+            os.path.dirname(importlib.import_module(app_name).__file__),
+            'training_data/{lang}/data.json'.format(lang=lang)
+        )
+        with open(data_path) as data:
+            training_data += json.load(data)
+    with NamedTemporaryFile(delete=False, mode="w", suffix=".json") as training_data_file:
+        click.echo("Generating training data file.")
+        training_data_file.write(
+            json.dumps({"rasa_nlu_data": {"common_examples": training_data}})
+        )
+    return training_data_file.name
 
 
 def _check_languages(languages):
@@ -50,6 +66,12 @@ def _check_languages(languages):
                     **{"lang": lang, "supported_languages": SUPPORTED_LANGUAGES}
                 )
             )
+
+
+def _load_config():
+    with open(os.path.join(_get_base_path(), "config/config.json")) as config_file:
+        config = json.load(config_file)
+    return config
 
 
 @click.group()
@@ -65,14 +87,17 @@ def cli():
 def train_models(languages):
     """Generate your trained model."""
     _check_languages(languages)
+    config = _load_config()
     for language in languages:
-        click.echo("Processing {lang}...".format(lang=language))
-        training_data = load_data(_get_training_data_path(language))
-        trainer = Trainer(RasaNLUConfig(_get_config_path(language)))
+        click.echo("================== Processing {lang} ==================".format(lang=language))
+        training_data = load_data(_get_training_data_path(language, config))
+        trainer = Trainer(RasaNLUConfig(cmdline_args=config))
+        click.echo("Training data {lang}.".format(lang=language))
         trainer.train(training_data)
+        click.echo("Persisting data for {lang}.".format(lang=language))
         model_dir = trainer.persist(_get_model_base_dir(language))
         click.echo("Stored data for {lang} in {path}.".format(lang=language, path=model_dir))
-
+    click.echo("================ Finished Training ================")
 
 @click.command()
 @click.option("languages", "--language", "-l", multiple=True, default=SUPPORTED_LANGUAGES,
@@ -81,13 +106,13 @@ def train_models(languages):
 def clear_models(languages):
     """Clear trained models."""
     for language in languages:
-        click.echo("Processing {lang}...".format(lang=language))
+        click.echo("Processing {lang}.".format(lang=language))
         model_base_dir = _get_model_base_dir(language)
         model_dirs = os.listdir(model_base_dir)
         for model_dir in model_dirs:
             model_path = os.path.join(model_base_dir, model_dir)
             if os.path.isdir(model_path):
-                click.echo("Deleting {model_path}...".format(model_path=model_path))
+                click.echo("Deleting {model_path}.".format(model_path=model_path))
                 shutil.rmtree(model_path)
 
 
